8d750354f28e11c0b7ea50f0f06ecc4e7ae8067f
David Peng <pengdaixin@kylinos.cn>
2020-05-21 10:40:38 +0800

    fixed issue

--- plugins/account/networkaccount/editpassdialog.cpp
+++ plugins/account/networkaccount/editpassdialog.cpp
@@ -514,16 +514,6 @@ bool EditPassDialog::eventFilter(QObject *w, QEvent *e) {
             setshow(content);
 
         }
-        if (e->type() == QEvent::FocusIn && pass_tips->isHidden()) {
-            pass_tips->show();
-
-            setshow(content);
-
-        } else if (e->type() == QEvent::FocusOut && !pass_tips->isHidden()) {
-            pass_tips->hide();
-
-            setshow(content);
-        }
     }
     if(w == valid_code) {
         if (e->type() == QEvent::FocusIn && !tips->isHidden()) {
