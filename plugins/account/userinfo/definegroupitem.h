33db5ccb34c35ae639647eb9600f080a1153a617
Yue Shuai <yueshuai@kylinos.cn>
2020-08-18 16:56:24 +0800

    add copyright
    Signed-off-by: Yue Shuai <yueshuai@kylinos.cn>

--- plugins/account/userinfo/definegroupitem.h
+++ plugins/account/userinfo/definegroupitem.h
@@ -1,6 +1,6 @@
 /* -*- Mode: C; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*-
  *
- * Copyright (C) 2019 Tianjin KYLIN Information Technology Co., Ltd.
+ * Copyright (C) 2020 KYLINOS Information Technology Co., Ltd.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -17,6 +17,7 @@
  * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA.
  *
  */
+
 #ifndef DEFINEGROUPITEM_H
 #define DEFINEGROUPITEM_H
 
