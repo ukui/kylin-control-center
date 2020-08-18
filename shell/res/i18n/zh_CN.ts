35a890a0e0c04cf2403fb508fb47b8fc83eb6b8e
zhouxiali <zhouxiali@kylinos.cn>
2020-08-18 16:55:02 +0800

    Solve the problem of incorrect font translation for cloud account and keyboard Settings #649

--- shell/res/i18n/zh_CN.ts
+++ shell/res/i18n/zh_CN.ts
@@ -2307,9 +2307,14 @@ folder will be deleted!</source>
         <translation>快</translation>
     </message>
     <message>
+<<<<<<< Updated upstream
         <location filename="../../../plugins/devices/keyboard/keyboardcontrol.ui" line="475"/>
         <location filename="../../../plugins/devices/keyboard/keyboardcontrol.cpp" line="119"/>
         <source>Test repetition rate of the input character:</source>
+=======
+        <location filename="../../../plugins/devices/keyboard/keyboardcontrol.ui" line="527"/>
+        <source>Input characters to test the repetition effect:</source>
+>>>>>>> Stashed changes
         <translation>输入字符测试重复效果：</translation>
         <extra-contents_path>/keyboard/Test repetition rate of the input character:</extra-contents_path>
     </message>
@@ -3843,7 +3848,7 @@ folder will be deleted!</source>
     </message>
     <message>
         <location filename="../../utils/functionselect.cpp" line="146"/>
-        <source>NetworkAccount</source>
+        <source>Cloud Account</source>
         <translation>云账户</translation>
     </message>
     <message>
@@ -5495,7 +5500,7 @@ Common reasons are that the overall screen size is too big, or you enabled more 
     <name>networkaccount</name>
     <message>
         <location filename="../../../plugins/account/networkaccount/networkaccount.cpp" line="25"/>
-        <source>NetworkAccount</source>
+        <source>Cloud Account</source>
         <translation>云账户</translation>
     </message>
 </context>
